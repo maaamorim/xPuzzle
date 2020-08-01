93638504774fe98c7b7fd8d0496735076e39f8bc
Mateus Antonio <mateuscfc8@outlook.com>
2020-08-01 23:01:05 +0000

    Adicionar layout inicial

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class XPuzzleApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,14 +28,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,20 +38,173 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "X-Puzzle",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    )
+                  ],
+                ),
+                flex: 1),
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('1')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('2')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('3')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('4')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                          ],
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('5')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('6')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('7')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('8')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                          ],
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('9')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('10')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('11')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('12')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                          ],
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('13')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('14')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('15')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  child: FlatButton(
+                                      onPressed: null, child: Text('')),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue))),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+                flex: 4),
+            Spacer(flex: 1)
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
